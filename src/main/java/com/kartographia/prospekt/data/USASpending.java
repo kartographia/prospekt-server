f43efbc1a90275a4123974563960dad1a78c382d
Peter Borissow <peter.borissow@kartographia.com>
2024-03-28 14:17:57 -0400

    Interim commit for USASpending

--- src/main/java/com/kartographia/prospekt/data/USASpending.java
+++ src/main/java/com/kartographia/prospekt/data/USASpending.java
@@ -4,8 +4,16 @@ import static com.kartographia.prospekt.data.Utils.*;
 import static com.kartographia.prospekt.queries.Index.getQuery;
 
 import java.util.*;
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
+
 import javaxt.sql.*;
+import javaxt.json.*;
+import javaxt.utils.ThreadPool;
 import static javaxt.utils.Console.console;
+
+import javaxt.express.utils.StatusLogger;
+import static javaxt.express.utils.StringUtils.*;
 
 
 public class USASpending {
@@ -61,6 +69,136 @@ public class USASpending {
 
     }
 
+
+  //**************************************************************************
+  //** updateCodes
+  //**************************************************************************
+  /** Used to populate the code look-up table
+   */
+    public static void updateCodes(Connection in, Connection out) throws Exception {
+
+        long sourceID = getOrCreateSource(out);
+
+        for (String type : new String[]{"action", "competition", "contract"}){
+
+
+          //Get existing codes
+            HashMap<String, JSONObject> codes = new HashMap<>();
+            for (javaxt.sql.Record record : out.getRecords(
+                "select id, key, value from code where source_id=" + sourceID +
+                " and type='" + type + "'")){
+                codes.put(record.get("key").toString(), record.toJson());
+            }
+
+
+          //Get codes from the source database
+            String sql = getQuery("usaspending", type + "_code").getSQL();
+            for (javaxt.sql.Record record : in.getRecords(sql)){
+                String str = record.get(0).toString();
+                int idx = str.indexOf(":");
+                String key = str.substring(0, idx).trim();
+                String val = str.substring(idx+1).trim();
+
+
+                JSONObject code = codes.get(key);
+                if (code==null){
+                    code = new JSONObject();
+                    code.set("key", key);
+                    code.set("value", val);
+                    codes.put(key, code);
+                }
+                else{
+                    String v = code.get("value").toString();
+                    if (v.equalsIgnoreCase(val)){
+                        codes.remove(key);
+                    }
+                    else{
+                        if (val.length()>v.length()){
+                            code.set("value", val);
+                        }
+                    }
+                }
+            }
+
+
+          //Create or update codes in our database
+            Iterator<String> it = codes.keySet().iterator();
+            while (it.hasNext()){
+                JSONObject code = codes.get(it.next());
+                Long id = code.get("id").toLong();
+
+
+                try (Recordset rs = out.getRecordset(
+                    "select * from code where id=" + (id==null ? -1 : id), false)){
+                    if (id==null){
+                        rs.addNew();
+                        rs.setValue("source_id", sourceID);
+                        rs.setValue("category", type);
+                    }
+                    rs.setValue("key", code.get("key"));
+                    rs.setValue("value", code.get("value"));
+                    rs.update();
+                }
+
+            }
+
+        }
+    }
+
+
+  //**************************************************************************
+  //** updateCompanies
+  //**************************************************************************
+  /** Used to update all the companies in the database
+   */
+    public static void updateCompanies(Database in, Database out, int numThreads) throws Exception {
+
+
+        AtomicLong recordCounter = new AtomicLong(0);
+        StatusLogger statusLogger = new StatusLogger(recordCounter);
+        long startTime = System.currentTimeMillis();
+
+        ThreadPool pool = new ThreadPool(numThreads){
+            public void process(Object obj){
+                String uei = (String) obj;
+
+                Connection conn = (Connection) get("conn", () -> {
+                    return in.getConnection();
+                });
+
+                try{
+                    //updateCompany(uei, conn, c2);
+                    recordCounter.incrementAndGet();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            public void exit(){
+                Connection conn = (Connection) get("conn");
+                if (conn!=null) conn.close();
+            }
+
+        }.start();
+
+
+        long t = 0;
+        String sql = getQuery("usaspending", "distinct_uei").getSQL();
+        try (Connection conn = in.getConnection()){
+            for (javaxt.sql.Record record : conn.getRecords(sql)){
+                pool.add(record.get(0).toString());
+                t++;
+            }
+        }
+        statusLogger.setTotalRecords(t);
+
+        pool.done();
+        pool.join();
+
+        statusLogger.shutdown();
+        console.log("Updated " + format(t) + " companies in " + getElapsedTime(startTime));
+    }
 
 
   //**************************************************************************
@@ -223,4 +361,328 @@ public class USASpending {
         }
     }
 
+
+  //**************************************************************************
+  //** updateAwards
+  //**************************************************************************
+  /** Used to update awards associated with a company
+   */
+    public static void updateAwards(String uei, Connection in, Connection out) throws Exception {
+
+        JSONObject prevAward = new JSONObject();
+        ArrayList<JSONObject> awards = new ArrayList<>();
+        String sql = getQuery("usaspending", "awards_by_company").getSQL().replace("{uei}", uei);
+        for (javaxt.sql.Record record : in.getRecords(sql)){
+
+
+          //Get award ID (sourceKey)
+            String awardID = getString(record.get("piid"));
+
+
+          //Get descriptive name of the award
+            String name = getString(record.get("award_description"));
+
+
+          //Build description
+            String description = "";
+            for (String key : new String[]{
+                "product_or_service_description",
+                "naics_description",
+                "commercial_item_acqui_desc"}){
+                String d = record.get(key).toString();
+                if (d!=null){
+                    d = d.trim();
+                    if (!d.isEmpty()){
+                        if (!d.endsWith(".")) d += ".";
+                        if (description.length()>0) description += " ";
+                        description += d;
+                    }
+                }
+            }
+            if (description.isEmpty()) description = null;
+
+
+
+          //Get type of award
+            String type = null;
+            if (in(record.get("type_of_contract_pricing"), "A,B,J,K,L,M")){
+                type = "Fixed Price";
+            }
+            else if (in(record.get("type_of_contract_pricing"), "S,T")){
+                type = "Cost";
+            }
+            else if (in(record.get("type_of_contract_pricing"), "R,U,V")){
+                type = "Cost Plus";
+            }
+            else if (in(record.get("type_of_contract_pricing"), "Y,Z")){
+                type = "T&M";
+            }
+
+
+          //Get value of the award
+            Double awardedValue = record.get("awarded_value").toDouble();
+            Double fundedValue = record.get("funded_value").toDouble();
+            Double extendedValue = record.get("extended_value").toDouble();
+
+
+
+
+          //Get naics code of the award
+            String naics = getString(record.get("naics"));
+
+
+
+          //Get customer and office
+            String customer = getString(record.get("customer"));
+            String office = getString(record.get("office"));
+
+
+          //Get dates
+            javaxt.utils.Date startDate = record.get("start_date").toDate();
+            javaxt.utils.Date endDate = record.get("end_date").toDate();
+            javaxt.utils.Date extendedDate = record.get("extended_date").toDate();
+
+
+          //Get competed
+            boolean competed = in(record.get("extent_competed"), "A,D,E,F,CDO");
+
+
+
+          //Create/update award
+            JSONObject award;
+            if (awardID.equals(prevAward.get("source_key").toString())){
+                award = prevAward;
+
+                //setIfNull("name", name, award);
+                setIfNull("description", description, award);
+                //setIfNull("date", date, award);
+                setIfNull("type", type, award);
+                setIfNull("naics", naics, award);
+
+
+                if (awardedValue!=null){
+                    Double v = award.get("awarded_value").toDouble();
+                    if (v!=null) award.set("awarded_value", Math.max(v, awardedValue));
+                }
+
+                if (extendedValue!=null){
+                    Double v = award.get("extended_value").toDouble();
+                    if (v!=null) award.set("extended_value", Math.max(v, extendedValue));
+                }
+
+                setIfNull("customer", customer, award);
+                setIfNull("office", office, award);
+
+                setIfNull("start_date", startDate, award);
+                javaxt.utils.Date s = award.get("start_date").toDate();
+                if (s!=null && s.isAfter(startDate)) award.set("start_date", startDate);
+
+                setIfNull("end_date", endDate, award);
+                javaxt.utils.Date e = award.get("end_date").toDate();
+                if (e!=null && e.isBefore(endDate)) award.set("end_date", endDate);
+
+                setIfNull("extended_date", extendedDate, award);
+                javaxt.utils.Date x = award.get("extended_date").toDate();
+                if (x!=null && x.isBefore(extendedDate)) award.set("extended_date", extendedDate);
+
+            }
+            else{
+
+              //Save previous award
+                if (prevAward.has("source_key")){
+                    awards.add(prevAward);
+                }
+
+
+                award = new JSONObject();
+                //award.set("name", name);
+                award.set("description", description);
+                //award.set("date", date);
+                award.set("type", type);
+                award.set("awarded_value", awardedValue);
+                //award.set("funded_value", fundedValue);
+                award.set("extended_value", extendedValue);
+                award.set("naics", naics);
+                award.set("customer", customer);
+                award.set("office", office);
+                award.set("start_date", startDate);
+                award.set("end_date", endDate);
+                award.set("extended_date", extendedDate);
+                award.set("competed", competed);
+                award.set("source_key", awardID);
+
+
+                JSONObject info = new JSONObject();
+                award.set("info", info);
+
+                JSONArray actions = new JSONArray();
+                info.set("actions", actions);
+            }
+
+
+
+          //Update award metadata
+            JSONObject info = award.get("info").toJSONObject();
+            info.set("solicitation_identifier", record.get("solicitation_identifier"));
+            String solicitationDate = record.get("solicitation_date").toString();
+            if (solicitationDate!=null){
+                solicitationDate = solicitationDate.replace("00:00:00", "").trim();
+                info.set("solicitation_date", solicitationDate);
+            }
+            info.set("award_key", record.get("unique_award_key"));
+            javaxt.utils.Date actionDate = record.get("action_date").toDate();
+            if (actionDate!=null){
+
+                String actionType = record.get("action_type").toString();
+                if (actionType==null) actionType = "-";
+
+                JSONObject action = new JSONObject();
+                action.set("date", actionDate.toString("yyyy-MM-dd"));
+                action.set("type", actionType);
+                action.set("desc", name);
+                action.set("funding", fundedValue);
+                JSONArray actions = info.get("actions").toJSONArray();
+                actions.add(action);
+            }
+
+            prevAward = award;
+        }
+        console.log("Found " + format(awards.size()) + " awards");
+
+
+
+      //Save awards
+        long sourceID = getOrCreateSource(out);
+        Long companyID;
+        javaxt.sql.Record r = out.getRecord("select id from company where uei='" + uei + "'");
+        if (r!=null) companyID = r.get(0).toLong();
+
+        for (JSONObject award : awards){
+            award.set("source_id", sourceID);
+
+
+            //Update title
+            JSONObject info = award.get("info").toJSONObject();
+            JSONArray actions = info.get("actions").toJSONArray();
+            TreeMap<String, String> titles = new TreeMap<>();
+            for (JSONValue a : actions){
+                String date = a.get("date").toString();
+                String desc = a.get("desc").toString();
+                titles.put(date, desc);
+            }
+            String firstDate = titles.firstKey();
+            award.set("name", titles.get(firstDate));
+            award.set("date", new javaxt.utils.Date(firstDate));
+            BigDecimal totalFunding = new BigDecimal(0);
+            Iterator<String> it = titles.keySet().iterator();
+            while (it.hasNext()){
+                String date = it.next();
+                for (JSONValue a : actions){
+                    if (a.get("date").toString().equals(date)){
+                        BigDecimal d = a.get("funding").toBigDecimal();
+                        if (d!=null){
+                            totalFunding = totalFunding.add(d);
+                        }
+                    }
+                }
+            }
+            award.set("funded_value", totalFunding);
+
+            System.out.println(award.toString(4));
+
+            if (true) continue;
+
+
+            String sourceKey = award.get("source_key").toString();
+            try (Recordset rs = out.getRecordset(
+                "select * from award where RECIPIENT_ID=" + companyID +
+                " and source_id=" + sourceID +
+                " and source_key='" + sourceKey + "'", false)){
+
+                if (rs.EOF) rs.addNew();
+                for (String key : award.keySet()){
+                    if (key.equals("info")){
+
+                    }
+                    else{
+                        rs.setValue(key, award.get(key));
+                    }
+                }
+                rs.update();
+            }
+        }
+
+    }
+
+
+    private static boolean in(javaxt.utils.Value v, String str){
+        String[] arr = str.split(",");
+        for (String s : arr) if (v.equals(s)) return true;
+        return false;
+    }
+
+
+    private static void setIfNull(String key, Object val, JSONObject json){
+        if (json.has(key)){
+            if (!json.isNull(key)) return;
+        }
+        json.set(key, val);
+    }
+
+
+    private static String getString(javaxt.sql.Value val){
+        String str = val.toString();
+        if (str!=null){
+            str = str.trim();
+            if (str.isEmpty()) str = null;
+        }
+        return str;
+    }
+
+
+  //**************************************************************************
+  //** getOrCreateSource
+  //**************************************************************************
+    private static Long getOrCreateSource(Connection conn) throws Exception {
+        String name = "usaspending.gov";
+
+        javaxt.sql.Record r = conn.getRecord(
+        "select id from source where name='" + name + "'");
+
+        if (r==null){
+            try (Recordset rs = conn.getRecordset(
+                "select * from source where id=-1", false)){
+                rs.addNew();
+                rs.setValue("name", name);
+                rs.update();
+                return rs.getGeneratedKey().toLong();
+            }
+        }
+        else{
+            return r.get(0).toLong();
+        }
+    }
+
+  //**************************************************************************
+  //** getOrCreateSource
+  //**************************************************************************
+    private static Long getOrCreateCompany(Connection conn) throws Exception {
+        String name = "usaspending.gov";
+
+        javaxt.sql.Record r = conn.getRecord(
+        "select id from source where name='" + name + "'");
+
+        if (r==null){
+            try (Recordset rs = conn.getRecordset(
+                "select * from source where id=-1", false)){
+                rs.addNew();
+                rs.setValue("name", name);
+                rs.update();
+                return rs.getGeneratedKey().toLong();
+            }
+        }
+        else{
+            return r.get(0).toLong();
+        }
+    }
 }
