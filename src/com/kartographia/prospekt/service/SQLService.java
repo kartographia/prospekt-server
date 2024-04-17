e714cd0450d1d4e43d8c83b5e6e77d8f613a7071
Peter Borissow <peter.borissow@kartographia.com>
2024-04-17 11:28:43 -0400

    Minor tweaks to the SQLService

--- src/com/kartographia/prospekt/service/SQLService.java
+++ src/com/kartographia/prospekt/service/SQLService.java
@@ -9,7 +9,6 @@ import javaxt.json.*;
 
 import javaxt.express.*;
 import javaxt.express.services.QueryService;
-import static javaxt.express.Config.getDatabase;
 import javaxt.express.notification.NotificationService;
 
 
@@ -32,11 +31,11 @@ public class SQLService extends WebService {
     public SQLService() throws Exception {
 
 
-        javaxt.io.Directory jobDir = getDirectory("jobDir");
+        javaxt.io.Directory jobDir = getDirectory("jobDir", true);
         if (jobDir==null || !jobDir.exists()){
             throw new IllegalArgumentException("Invalid \"jobDir\" defined in config file");
         }
-        javaxt.io.Directory logDir = getDirectory("logDir");
+        javaxt.io.Directory logDir = getDirectory("logDir", false);
 
 
 
@@ -50,15 +49,15 @@ public class SQLService extends WebService {
 
 
       //Instantiate query service for the awards database
-        Database awardsDatabase = getDatabase(Config.get("sources").get("usaspending.gov").get("database"));
-        if (awardsDatabase!=null){
-            awardsDatabase.enableMetadataCache(true);
-            awardsDatabase.initConnectionPool();
-            queryServices.put("awards", new QueryService(awardsDatabase, jobDir, logDir){
+        try {
+            queryServices.put("awards", new QueryService(Config.getAwardsDatabase(), jobDir, logDir){
                 public void notify(QueryService.QueryJob job){
                     NotificationService.notify(job.getStatus(), "SQL", new javaxt.utils.Value(job));
                 }
             });
+        }
+        catch(Exception e){
+            console.log("Failed to initialize awards database");
         }
     }
 
@@ -82,35 +81,41 @@ public class SQLService extends WebService {
   //**************************************************************************
   //** getDirectory
   //**************************************************************************
-    private static javaxt.io.Directory getDirectory(String key){
+    private static javaxt.io.Directory getDirectory(String key, boolean create){
         javaxt.io.Directory dir = null;
         JSONObject config = Config.get("webserver").toJSONObject();
 
+
+      //Set path (string)
         String path;
         if (config.has(key)){
             path = config.get(key).toString();
         }
         else{
             javaxt.io.Directory web = new javaxt.io.Directory(config.get("webDir").toString());
-            if (key.endsWith("Dir")){
-                key = key.substring(0, key.length()-3) + "s";
+            if (key.endsWith("Dir")){ //e.g. jobDir, logDir
+                key = key.substring(0, key.length()-3) + "s"; //e.g. jobs, logs
+
+              //Update "jobs" key. Rename to "temp". Looks a little cleaner...
+                if (key.endsWith("jobs")) key = key.substring(0, key.length()-4) + "temp";
             }
+
             path = web.getParentDirectory() + key;
         }
 
 
 
-
+      //Set directory. Create new one as needed
         if (path!=null && !path.isEmpty()){
             dir = new javaxt.io.Directory(path);
-            if (!dir.exists()) dir.create();
+            if (!dir.exists() && create) dir.create();
             if (dir.exists()){
                 dir = new javaxt.io.Directory(dir.toString() + "sql");
-                if (!dir.exists()) dir.create();
+                if (!dir.exists() && create) dir.create();
             }
             if (!dir.exists()) dir = null;
         }
 
-        return dir;
+        return dir; //may return null, which is ok for logs
     }
 }
