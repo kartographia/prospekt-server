150ac6c8d84f8e65562d802549eb9355e7f2f62b
Peter Borissow <peter.borissow@kartographia.com>
2024-06-24 19:13:23 -0400

    Added user icons and updated handling of CRUD events in the BookmarkView

--- web/app/companies/BookmarkView.js
+++ web/app/companies/BookmarkView.js
@@ -22,6 +22,7 @@ prospekt.companies.BookmarkView = function(parent, config) {
     var callout;
     var bookmarkEditor;
     var bookmarks = {};
+    var userIcons = {};
 
 
   //**************************************************************************
@@ -52,6 +53,7 @@ prospekt.companies.BookmarkView = function(parent, config) {
   //**************************************************************************
     this.clear = function(){
         bookmarks = {};
+        userIcons = {};
         panel.clear();
     };
 
@@ -69,6 +71,7 @@ prospekt.companies.BookmarkView = function(parent, config) {
                 bookmarks.forEach((bookmark)=>{
                     addBookmark(bookmark);
                 });
+                updateUserIcons();
                 panel.update();
             },
             failure: function(){
@@ -90,11 +93,19 @@ prospekt.companies.BookmarkView = function(parent, config) {
     this.notify = function(op, model, id, userID){
 
         if (model==="CompanyGroup"){
+
+          //Hide the bookmarkEditor as needed
+            if (bookmarkEditor && bookmarkEditor.id===id){
+                bookmarkEditor.clear();
+                bookmarkEditor.hide();
+            }
+
             if (op==="delete"){
                 var bookmark = bookmarks[id+""];
                 if (bookmark){
                     panel.innerDiv.removeChild(bookmark.card);
                     panel.update();
+                    delete bookmarks[id+""];
                 }
             }
             else{
@@ -103,21 +114,39 @@ prospekt.companies.BookmarkView = function(parent, config) {
                         var bookmark = JSON.parse(text);
                         if (op==="create"){
                             addBookmark(bookmark);
+                            updateUserIcons();
                             panel.update();
                         }
                         else if (op==="update"){
                             var orgBookmark = bookmarks[bookmark.id+""];
-                            var card = orgBookmark.card;
-                            orgBookmark = JSON.parse(JSON.stringify(orgBookmark));
-                            delete orgBookmark["card"];
-                            if (isDirty(bookmark, orgBookmark)){
-                                card.update(bookmark);
-                                bookmarks[bookmark.id+""] = bookmark;
-                                bookmark.card = card;
+                            if (orgBookmark){
+                                var card = orgBookmark.card;
+                                orgBookmark = JSON.parse(JSON.stringify(orgBookmark));
+                                delete orgBookmark["card"];
+                                if (isDirty(bookmark, orgBookmark)){
+                                    card.update(bookmark);
+                                    bookmarks[bookmark.id+""] = bookmark;
+                                    bookmark.card = card;
+                                    updateUserIcons();
+                                }
+                            }
+                            else{
+                                addBookmark(bookmark);
+                                updateUserIcons();
+                                panel.update();
                             }
                         }
                     },
-                    failure: function(){} //ignore
+                    failure: function(request){
+                        if (request.status==403){
+                            var bookmark = bookmarks[id+""];
+                            if (bookmark){
+                                panel.innerDiv.removeChild(bookmark.card);
+                                panel.update();
+                                delete bookmarks[id+""];
+                            }
+                        }
+                    }
                 });
             }
         }
@@ -163,16 +192,24 @@ prospekt.companies.BookmarkView = function(parent, config) {
 
 
           //Render users
+            users.innerHTML = "";
+            bookmark.users.sort((a,b)=>{
+                return b.accessLevel-a.accessLevel;
+            });
             for (var i=0; i<bookmark.users.length; i++){
-                var img = createElement("div", users, "user");
-                //img.style.backgroundImage = "url(/images/people/" + user + ".jpg)";
                 if (i==3){
+                    var d = createElement("div", users, "user overflow");
+                    d.innerText = "+" + (bookmark.users.length-i);
                     break;
                 }
                 else{
-
+                    var userID = bookmark.users[i].userID;
+                    var userIcon = userIcons[userID+""];
+                    if (!userIcon) userIcon = userIcons[userID+""] = [];
+                    userIcon.push(createElement("div", users, "user"));
                 }
             }
+
 
           //Render title
             title.innerText = bookmark.name;
@@ -227,17 +264,11 @@ prospekt.companies.BookmarkView = function(parent, config) {
                         name: "Save",
                         onclick: function(){
                             bookmarkEditor.validateFields((bookmark)=>{
-
-                                console.log(bookmark);
-
-
                                 post("CompanyGroup", bookmark, {
                                     success: function(){
                                         win.close();
                                     }
                                 });
-
-
                             });
                         }
                     },
@@ -256,8 +287,11 @@ prospekt.companies.BookmarkView = function(parent, config) {
 
             bookmarkEditor = new prospekt.companies.BookmarkEditor(body, config);
             bookmarkEditor.show = win.show;
+            bookmarkEditor.hide = win.hide;
         }
 
+
+        bookmarkEditor.id = bookmark.id; //hook for notify() method
         bookmarkEditor.update(bookmark);
         bookmarkEditor.show();
     };
@@ -320,9 +354,6 @@ prospekt.companies.BookmarkView = function(parent, config) {
             editBookmark(bookmark, target.parentNode);
         }));
         if (accessLevel===3){
-            div.appendChild(createMenuOption("Manage Access", "users", function(){
-                //deleteBookmark(bookmark, target.parentNode);
-            }));
             div.appendChild(createMenuOption("Delete Bookmark", "trash", function(){
                 deleteBookmark(bookmark, target.parentNode);
             }));
@@ -359,6 +390,34 @@ prospekt.companies.BookmarkView = function(parent, config) {
     };
 
 
+  //**************************************************************************
+  //** updateUserIcons
+  //**************************************************************************
+    var updateUserIcons = function(){
+
+        var userIDs = Object.keys(userIcons);
+        if (userIDs.length===0) return;
+
+        get("users?id="+userIDs.join(",") +
+            "&active=true&fields=id,firstName,lastName,info", {
+            success: function(str){
+                var arr = parseResponse(str);
+                arr.forEach((user)=>{
+                    var arr = userIcons[user.id+""];
+                    if (arr) arr.forEach((div)=>{
+                        if (user.info && user.info.profilePic){
+                            div.style.backgroundImage = user.info.profilePic;
+                        }
+                        else{
+                            div.innerText = user.firstName.substring(0,1);
+                        }
+                    });
+
+                });
+            }
+        });
+    };
+
 
   //**************************************************************************
   //** Utils
@@ -372,6 +431,7 @@ prospekt.companies.BookmarkView = function(parent, config) {
     var get = javaxt.dhtml.utils.get;
 
     var createOverflowPanel = prospekt.utils.createOverflowPanel;
+    var parseResponse = prospekt.utils.parseResponse;
     var createWindow = prospekt.utils.createWindow;
     var getCallout = prospekt.utils.getCallout;
     var addCommas = prospekt.utils.addCommas;
