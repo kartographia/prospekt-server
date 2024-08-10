1fdd3733e4fdd7e80c5ea0891561b29817f26b9b
Peter Borissow <email@pborissow.dev>
2024-08-09 22:44:44 -0400

    Added progress sliders and opportunity link to AwardDetails

--- web/app/awards/AwardDetails.js
+++ web/app/awards/AwardDetails.js
@@ -70,7 +70,7 @@ prospekt.awards.AwardDetails = function(parent, config) {
         var rightCol = tr.addColumn({height: "100%"});
 
 
-      //Create stats
+      //Create stats in the left column
         var table = createTable(leftCol);
         table.style.height = "";
         table.className = "company-overview";
@@ -87,10 +87,16 @@ prospekt.awards.AwardDetails = function(parent, config) {
                 verticalAlign: "top"
             });
 
-
             col.setValue = function(value){
-                col.innerText = value;
+                if (isElement(value)){
+                    col.innerHTML = "";
+                    col.appendChild(value);
+                }
+                else{
+                    col.innerText = value;
+                }
             };
+
             col.setValue(value);
 
             rows[key] = {
@@ -113,7 +119,10 @@ prospekt.awards.AwardDetails = function(parent, config) {
             "Funded",
 
             "Extended Date",
-            "Extended Value"
+            "Extended Value",
+
+            "Solicitation",
+            "Solicitation Date"
 
         ].forEach((key)=>{
             addRow(key, "-");
@@ -124,13 +133,34 @@ prospekt.awards.AwardDetails = function(parent, config) {
         rows["Transactions"].setValue = function(){};
 
 
+      //Create graphics in the right column
         var div = createElement("div", rightCol, {
-            width: "300px",
+            width: "200px",
             height: "100%"
         });
         var logo = createElement("div", div, "customer-logo");
 
-
+        var d = createElement("div", div, "award-progress");
+        var table = createTable(d);
+        table.style.height = "";
+        var addProgressMeter = function(label){
+            var tr = table.addRow();
+            var td = tr.addColumn("label");
+            td.innerText = label;
+            td = tr.addColumn({ width: "100%" });
+            var groove = createElement("div", td, "groove");
+            var slider = createElement("div", groove, "slider");
+            slider.style.width = "0%";
+            slider.style.height = "100%";
+            return {
+                update: function(percent){
+                    percent = Math.min(percent, 1);
+                    slider.style.width = percent*100.0 + "%";
+                }
+            };
+        };
+        var funding = addProgressMeter("Funding");
+        var pop = addProgressMeter("POP");
 
 
         overview = {};
@@ -171,14 +201,65 @@ prospekt.awards.AwardDetails = function(parent, config) {
                         if (updateNeg) val = "-" + val.replace("-","");
                     }
 
+
+                  //Special case for solicitation
+                    if (key==="solicitation"){
+                        val = award.sourceKey;
+                        if (award.info){
+                            var awardKey = award.info.award_key;
+                            console.log(awardKey);
+                            if (awardKey && !val) val = awardKey;
+                        }
+                        if (award.sourceKey){
+                            var url = "https://sam.gov/search/?index=opp&page=1&pageSize=25&sort=-modifiedDate" +
+                            "&sfm%5Bstatus%5D%5Bis_active%5D=true&sfm%5Bstatus%5D%5Bis_inactive%5D=true" +
+                            "&sfm%5BsimpleSearch%5D%5BkeywordRadio%5D=ALL" +
+                            "&sfm%5BsimpleSearch%5D%5BkeywordTags%5D%5B0%5D%5Bkey%5D=" + award.sourceKey +
+                            "&sfm%5BsimpleSearch%5D%5BkeywordTags%5D%5B0%5D%5Bvalue%5D=" + award.sourceKey;
+                            var link = createElement("a");
+                            link.href = url;
+                            link.target = "_blank";
+                            link.innerText = val;
+                            val = link;
+                        }
+                    }
+                    else if (key==="solicitationDate"){
+                        val = "-";
+                        if (award.info){
+                            if (award.info.solicitation_date){
+                                val = moment(award.info.solicitation_date).format("YYYY-MM-DD");
+                            }
+                        }
+                    }
+
+
                     row.setValue(val);
                 }
             }
 
 
-            logo.style.backgroundImage = "url(/images/logos/" + award.customer.toLowerCase() + ".svg)";
+
+            if (award.startDate && award.endDate){
+                var startDate = moment(award.startDate);
+                var endDate = moment(award.endDate);
+                var today = new moment();
+                var totalDays = endDate.diff(startDate, 'days', true);
+                pop.update(today.diff(startDate, 'days', true)/totalDays);
+            }
+            else{
+               pop.update(0);
+            }
 
 
+            if (award.value){
+                funding.update(parseFloat(award.funded)/parseFloat(award.value));
+            }
+            else{
+                funding.update(0);
+            }
+
+
+            addCustomerLogo(award.customer, logo);
 
         };
     };
@@ -252,13 +333,14 @@ prospekt.awards.AwardDetails = function(parent, config) {
     var createElement = javaxt.dhtml.utils.createElement;
     var createTable = javaxt.dhtml.utils.createTable;
     var addShowHide = javaxt.dhtml.utils.addShowHide;
+    var isElement = javaxt.dhtml.utils.isElement;
     var isString = javaxt.dhtml.utils.isString;
     var merge = javaxt.dhtml.utils.merge;
 
 
     var addCommas = prospekt.utils.addCommas;
     var getActionCodes = prospekt.utils.getActionCodes;
-
+    var addCustomerLogo = prospekt.utils.addCustomerLogo;
 
     init();
 
